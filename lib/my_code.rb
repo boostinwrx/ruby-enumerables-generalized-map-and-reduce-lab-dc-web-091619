acdbc4247305fa1a94a106777be9d23625f2bca8
Antonio Abraham <tony.calablava@gmail.com>
2019-08-16 06:49:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
-# Your Code Here
+def map(source_array)
+  new = [ ]
+  i = 0 
+  while i < source_array.length 
+   new.push(yield source_array[i])
+   i+=1
+end
+new
+end
+
+def reduce(source_array,starting_point = nil )
+  
+  
